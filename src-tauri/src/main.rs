b9675b82c6ebf54558632e7e9a47adc8fcb6e5fc
xtc <xtc@example.com>
2026-06-16 21:02:54 +0800

    fix: move deepLink to plugins in tauri.conf.json, add deep link plugin dependencies, and bump version to v0.1.2

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -21,6 +21,7 @@ fn main() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::default().build())
         .setup(move |app| {
